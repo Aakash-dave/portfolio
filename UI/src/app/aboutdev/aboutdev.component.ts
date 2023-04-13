84f6898215118a637c1c94c4c10070000d9e49c8
Aakash Dave <aakash.dave81@gmail.com>
2023-04-13 22:09:20 +0530

    update responsive design inner sizing

--- UI/src/app/aboutdev/aboutdev.component.ts
+++ UI/src/app/aboutdev/aboutdev.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-aboutdev',
@@ -8,6 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class AboutdevComponent implements OnInit {
 
   constructor() { }
+
+
+  @HostListener('window:resize') screenResize() {
+    let div = <HTMLElement>document.getElementsByClassName('parent-container')[0];
+    div.style.height = window.innerHeight.toString().concat('px');
+    div.style.width = window.innerWidth.toString().concat('px');
+  }
 
   ngOnInit(): void {
   }
--- UI/src/app/app.component.ts
+++ UI/src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(): void {
-
+    console.log(window.innerWidth, window.innerHeight);
   }
 
 }
